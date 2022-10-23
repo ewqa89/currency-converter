b124e3f330d01d1aa2eb58678e408c2b1039c909
Ewa Paszkowska <ewqa89@gmail.com>
2022-10-23 21:54:19 +0200

    added actual value exchange

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
-let formElement = document.querySelector(".js-form");
-let valueElement = document.querySelector(".js-value");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const currencyElement = document.querySelector(".js-currency");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-    let value = valueElement.value;
-    let currency = currencyElement.value;
+        const valueElement = document.querySelector(".js-value");
+        const resultElement = document.querySelector(".js-result");
 
-    let result = value * currency;
+        const value = valueElement.value;
+        const currency = currencyElement.value;
 
-    resultElement.innerText = result.toFixed(2);
-});
+        const result = value * currency;
 
+        resultElement.innerText = result.toFixed(2);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    const showCourse = () => {
+        const exchangeChoose = document.querySelector(".js-exchangeChoose");
+
+        exchangeChoose.innerText = (`*Kurs: ${currencyElement.value}`);
+
+        currencyElement.addEventListener("change", () => {
+
+            const currencyElement2 = document.querySelector(".js-currency");
+            exchangeChoose.innerText = (`*Kurs: ${currencyElement2.value}`);
+        });
+    }
+
+    init();
+    showCourse();
+}
